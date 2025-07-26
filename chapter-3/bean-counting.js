c9d5d176770d8246b18f4e1a428ae1af0363e991
sanjanaa2102 <sanjanaprakash216@gmail.com>
2025-07-26 22:41:05 +0530

    Create bean-counting.js

---
+++ chapter-3/bean-counting.js
@@ -0,0 +1,20 @@
+function countChar(string, ch) 
+{
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) 
+  {
+    if (string[i] == ch) 
+    {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string)
+{
+  return countChar(string, "B");
+}
+console.log(countBs("BASKETBALL"));
+
+console.log(countChar("MISSISSIPPI", "S"));
